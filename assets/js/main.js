10094df7f161bac0ebc977dc4a01b0f11fb6c4da
johnnhenrique <johnnhenrique@gmail.com>
2024-11-27 15:19:43 -0300

    Atualização de Cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,63 @@
-window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', onScroll);
 
-onScroll()
+onScroll();
 
 function onScroll() {
-    showNavOnScroll()
-    showBackToTopButtonOnScroll()
-    activateMenuAtCurrentSection(home)
-    activateMenuAtCurrentSection(services)
-    activateMenuAtCurrentSection(about)
+    showNavOnScroll();
+    showBackToTopButtonOnScroll();
+    activateMenuAtCurrentSection(home);
+    activateMenuAtCurrentSection(services);
+    activateMenuAtCurrentSection(about);
 }
 
 function activateMenuAtCurrentSection(section) {
-    const targetLine = scrollY + innerHeight / 2
+    const targetLine = scrollY + innerHeight / 2;
 
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
 
-    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
 
-    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndsAt = sectionTop + sectionHeight;
 
-    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
 
-    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
 
-    const sectionId = section.getAttribute('id')
-    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu a[href*="#${sectionId}"]`);
 
-    menuElement.classList.remove('active')
-    if (sectionBoundaries) {
-        menuElement.classList.add('active')
+    if (menuElement) {
+        menuElement.classList.remove('active');
+        if (sectionBoundaries) {
+            menuElement.classList.add('active');
+        }
     }
 }
 
 function showNavOnScroll() {
     if (scrollY > 0) {
-        navigation.classList.add('scroll')
+        navigation.classList.add('scroll');
     } else {
-        navigation.classList.remove('scroll')
+        navigation.classList.remove('scroll');
     }
 }
 
 function showBackToTopButtonOnScroll() {
     if (scrollY > 500) {
-        backToTopButton.classList.add('show')
+        backToTopButton.classList.add('show');
     } else {
-        backToTopButton.classList.remove('show')
+        backToTopButton.classList.remove('show');
     }
 }
 
+
 function openMenu() {
-    document.body.classList.add('menu-expanded')
+    document.body.classList.add('menu-expanded');
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+    document.body.classList.remove('menu-expanded');
 }
 
 ScrollReveal({
